d3febfcdd8ced5197243ddde53ec9fc8510657b2
William Oliveira <sudowilliam@gmail.com>
2014-01-07 02:54:03 -0200

    implements totalCart function

--- app/controllers/cartController.js
+++ app/controllers/cartController.js
@@ -1,6 +1,15 @@
 App.controller('CartController', function ($scope) { 
   $scope.bill = {};
-  
+
+  $scope.totalCart = function() {
+    var total = 0;
+    for (var i = 0, len = $scope.products.length; i < len; i++) {
+      total = total + $scope.products[i].price * $scope.products[i].qty;
+    }
+    
+    return total;
+  }
+
   $scope.remove = function(index) {
     $scope.products.splice(index, 1);
   }
