f9e803b78ad301d3a1e9ccae6b0e05f898ba6952
William Oliveira <sudowilliam@gmail.com>
2014-01-07 02:52:57 -0200

    implementing factory

--- app/modules/shoppingModule.js
+++ app/modules/shoppingModule.js
@@ -1,1 +1,16 @@
 var shoppingModule = angular.module('ShoppingModule', []);
+
+shoppingModule.factory('Products', function() {
+  var products = {};
+
+  products.query = function() {
+    return [
+      { name: 'Product 1', price: '22', qty: 0 },
+      { name: 'Product 2', price: '32', qty: 0 },
+      { name: 'Product 3', price: '42', qty: 0 },
+      { name: 'Product 4', price: '52', qty: 0 }
+    ];
+  }
+
+  return products;
+});
